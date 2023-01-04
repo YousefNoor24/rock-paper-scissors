1dfd1eee10a245a32f16f163a84fcf50267dfb44
YousefNoor24 <yousef.noor1616@gmail.com>
2023-01-05 01:03:25 +0800

    Edit playRound() and game() in script.js

--- script.js
+++ script.js
@@ -25,19 +25,19 @@ function playRound(playerSelection, computerSelection) {
     const playerChoice = playerSelection.toLowerCase();
 
     if (playerChoice === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock."
+        return "You Lose! Paper beats Rock.";
     } else if (playerChoice === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors."
+        return "You Win! Rock beats Scissors.";
     } else if (playerChoice === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beat Paper."
+        return "You Lose! Scissors beat Paper.";
     } else if (playerChoice === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock."
+        return "You Win! Paper beats Rock.";
     } else if (playerChoice === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors."
+        return "You Lose! Rock beats Scissors.";
     } else if (playerChoice === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper."
+        return "You Win! Scissors beats Paper.";
     } else if (playerChoice === computerSelection) {
-        return "Its a Draw!" 
+        return "Its a Draw! " + capitalizeFirstLetter(playerChoice) + " is equal to " + capitalizeFirstLetter(computerSelection); 
     } else {
         return "Please choose Rock, Paper, or Scissors";
     }
@@ -49,7 +49,7 @@ function game() {
     const computerChoice = getComputerChoice();
 
     for (let i = 1; i <= 5; i++) {
-        playerChoice = prompt("Type in Rock, Paper, or Scissors")
+        playerChoice = prompt("Type in Rock, Paper, or Scissors");
         console.log("Round " + i + ": " + playRound(playerChoice, computerChoice));    
     }
 }
