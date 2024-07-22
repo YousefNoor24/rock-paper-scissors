9b521b7d86a71e45b9e53191c8a9e973b4763bdf
YousefNoor24 <yousef.noor1616@gmail.com>
2024-07-22 11:32:48 +0800

    Create getHumanChoice() in script.js

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let humanChoice = prompt("Please choose between Rock ('R' or 'r'), Paper ('P' or 'p'), or Scissors ('S' or 's').");
+
+    if (humanChoice === 'R' || humanChoice === 'r') {
+        return "Rock";
+    } else if (humanChoice === 'P' || humanChoice === 'p') {
+        return "Paper";
+    } else if (humanChoice === 'S' || humanChoice === 's') {
+        return "Scissors";
+    } else {
+        humanChoice = prompt("Please choose a vaild choice between Rock ('R' or 'r'), Paper ('P' or 'p'), or Scissors ('S' or 's').");
+    }
+}
+
+console.log(getHumanChoice());
