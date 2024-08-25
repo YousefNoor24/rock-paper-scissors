f15d1a65262bf04025e55d169784e0e2ca7870b7
YousefNoor24 <yousef.noor1616@gmail.com>
2024-08-25 19:52:38 +0800

    Edit script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let humanScore = 0;
-let computerScore = 0;
+function humanScored() {
+    return 0;
+}
+
+function computerScored() {
+    return 1;
+}
 
 function getComputerChoice(){
     const randomNum = Math.floor(Math.random() * 3);
@@ -17,7 +22,7 @@ function getHumanChoice(){
     const rockBtn = document.querySelector("#rock");
     const paperBtn = document.querySelector("#paper");
     const scissorsBtn = document.querySelector("#scissors");
-
+    
     rockBtn.addEventListener("click", () => {
         console.log("Rock button is clicked.");
         return "Rock";
@@ -32,36 +37,77 @@ function getHumanChoice(){
         console.log("Scissors button is clicked.");
         return "Scissors";
     });
+    
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, computerScore, humanScore, announcer) {
     if (humanChoice === "Rock" && computerChoice === "Paper") {
-        computerScore++;
-        return "You lose! Rock lose to paper.";
+        announcer.textContent = "You lose! Rock lose to paper.";
+        return computerScored();
     } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-        humanScore++;
-        return "You win! Rock beats scissors.";
+        announcer.textContent = "You win! Rock beats scissors.";
+        return humanScored();
     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        humanScore++;
-        return "You win! Paper beats rock.";
+        announcer.textContent = "You win! Paper beats rock.";
+        return humanScored();
     } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-        computerScore++;
-        return "You lose! Paper lose to scissors.";
+        announcer.textContent  = "You lose! Paper lose to scissors.";
+        return computerScored();
     } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-        computerScore++;
-        return "You lose! Scissors lose to rock.";
+        announcer.textContent  = "You lose! Scissors lose to rock.";
+        return computerScored();
     } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-        humanScore++;
-        return "You win! Scissors beat paper."
+        announcer.textContent  = "You win! Scissors beat paper."
+        return humanScored();
     } else {
-        return "Tie! " + humanChoice + " is equal to " + computerChoice;
+        announcer.textContent  = "Tie! " + humanChoice + " is equal to " + computerChoice;
     }
 }
 
 
 function playGame() {
-    
+    const computerScoreDiv = document.querySelector("#computer-score");
+    const humanScoreDiv = document.querySelector("#human-score");
+    const announcer = document.querySelector("#announcer");
+    const choice = document.querySelector('#human-choice').querySelectorAll('button');
+
+    let computerScore = 0;
+    let humanScore = 0;
+    let humanChoice = "";
+    let computerChoice = "";
+    let gameOn = true;
+
+    choice.forEach((button) => {
+
+        button.addEventListener('click', () => {
+            if (gameOn) {
+                humanChoice = button.value;
+                computerChoice = getComputerChoice();
+                roundPlay = playRound(humanChoice, computerChoice, computerScore, humanScore, announcer);
+                roundPlay;
+                if (roundPlay === humanScored()) {
+                    humanScore++;
+                    humanScoreDiv.textContent = `Human Score: ${humanScore}`;
+
+                } 
+                
+                if (roundPlay === computerScored()) {
+                    computerScore++;
+                    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+                }
+
+                if (humanScore >= 5 ) {
+                    announcer.textContent = "Game Over! Congrats you won."
+                    gameOn = false;
+                }
+                if (computerScore >= 5) {
+                    announcer.textContent = "Game Over! You lose."
+                    gameOn = false;
+                }
+            }
+        });
+    });
     
 }
  
-getHumanChoice();
+playGame();
